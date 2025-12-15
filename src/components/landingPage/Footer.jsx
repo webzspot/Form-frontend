a55ffdfea344e7325fc3b3c3d335203f6903a061
Antonyamose <antonyamose3@gmail.com>
2025-12-15 10:31:02 +0530

    user

--- src/components/landingPage/Footer.jsx
+++ src/components/landingPage/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaTwitter, FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
